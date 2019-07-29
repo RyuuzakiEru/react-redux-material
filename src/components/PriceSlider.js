edb0008a0c7159b2cf8573c2b1de76bffe94a4af
Ryuuzaki <rmorales82@outlook.com>
2019-07-29 12:53:30 +0200

    add debounce for handleChange

--- src/components/PriceSlider.js
+++ src/components/PriceSlider.js
@@ -1,120 +1,112 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
 
-const useStyles = makeStyles(theme => ({
+import debounce from 'debounce';
+
+const useStyles = makeStyles(() => ({
 	root: {
-        display: 'flex',
-        flexFlow: 'column',
-        flexGrow: 1,
-        justifyContent: 'space-around',
-        boxShadow: 'inherit'
+		display: 'flex',
+		flexFlow: 'column',
+		flexGrow: 1,
+		justifyContent: 'space-around',
+		boxShadow: 'inherit'
 	}
 }));
 
-const  ValueLabelComponent = (props) => {
-
-	return (
-		<Tooltip
-            open={true}
-            placement='bottom'
-			enterTouchDelay={0}
-			title={`${props.value} $`}
-
-        />
-	);
-}
-
 const iOSBoxShadow =
-  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+	'0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
 const marks = [
-  {
-    value: 0,
-  },
-  {
-    value: 50,
-  },
-  {
-    value: 100,
-  },
-  {
-    value: 150,
-  },
-  {
-    value: 200,
-  },
+	{
+		value: 0
+	},
+	{
+		value: 50
+	},
+	{
+		value: 100
+	},
+	{
+		value: 150
+	},
+	{
+		value: 200
+	}
 ];
 
 const CustomSlider = withStyles({
-    root: {
-      color: '#19857b',
-      height: 2,
-      padding: '15px 0',
-    },
-    thumb: {
-      height: 28,
-      width: 28,
-      backgroundColor: '#fff',
-      boxShadow: iOSBoxShadow,
-      marginTop: -10,
-      marginLeft: -14,
-      '&:focus,&:hover,&$active': {
-        boxShadow: '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
-        // Reset on touch devices, it doesn't add specificity
-        '@media (hover: none)': {
-          boxShadow: iOSBoxShadow,
-        },
-      },
-    },
-    active: {},
-    valueLabel: {
-      left: 'calc(-50% + 11px)',
-      top: -20,
-      '& *': {
-        background: 'transparent',
-        color: '#000',
-      },
-    },
-    track: {
-      height: 2,
-    },
-    rail: {
-      height: 2,
-      opacity: 0.5,
-      backgroundColor: '#bfbfbf',
-    },
-    mark: {
-      backgroundColor: '#bfbfbf',
-      height: 30,
-      width: 2,
-      marginTop: -14,
-    },
-    markActive: {
-      backgroundColor: 'currentColor',
-    },
-  })(Slider);
+	root: {
+		color: '#19857b',
+		height: 2,
+		padding: '15px 0'
+	},
+	thumb: {
+		height: 28,
+		width: 28,
+		backgroundColor: '#fff',
+		boxShadow: iOSBoxShadow,
+		marginTop: -10,
+		marginLeft: -14,
+		'&:focus,&:hover,&$active': {
+			boxShadow:
+				'0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+			// Reset on touch devices, it doesn't add specificity
+			'@media (hover: none)': {
+				boxShadow: iOSBoxShadow
+			}
+		}
+	},
+	active: {},
+	valueLabel: {
+		left: 'calc(-50% + 11px)',
+		top: -20,
+		'& *': {
+			background: 'transparent',
+			color: '#000'
+		}
+	},
+	track: {
+		height: 2
+	},
+	rail: {
+		height: 2,
+		opacity: 0.5,
+		backgroundColor: '#bfbfbf'
+	},
+	mark: {
+		backgroundColor: '#bfbfbf',
+		height: 30,
+		width: 2,
+		marginTop: -14
+	},
+	markActive: {
+		backgroundColor: 'currentColor'
+	}
+})(Slider);
 
-const PriceSlider = (props) => {
+const PriceSlider = props => {
 	const classes = useStyles();
 
+	const handleChange = (event, values) => {
+		console.log(values);
+	};
 	return (
 		<Paper display="flex" className={classes.root}>
-			<Typography >Price Range</Typography>
+			<Typography>Price Range</Typography>
 			<CustomSlider
-                valueLabelDisplay="on"
-                valueLabelFormat= { val => `${val} $`}
-                marks={marks}
+				valueLabelDisplay="on"
+				valueLabelFormat={val => `${val} $`}
+				marks={marks}
 				aria-label="price"
+				onChange={debounce(handleChange, 300)}
 				min={0}
 				max={200}
 				defaultValue={[10, 100]}
 			/>
 		</Paper>
 	);
-}
+};
 
 export default PriceSlider;
